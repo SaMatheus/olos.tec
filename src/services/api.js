f3aad264a9faaa3f382c6e6b10964a5fb8a10668
Matheus Sá <matheus.sa.alves@outlook.com>
2021-05-14 21:33:22 -0300

    Corrigindo baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -3,5 +3,5 @@ import axios from 'axios'
 export const apiKey = '4fUo1vx6eY0JIfeYoaMxrlGlWlM8ZpcoTzi7rQWs'
 
 export const api = axios.create({
-  baseUrl:'https://api.nasa.gov/neo/rest/v1/feed?'
+  baseURL:'https://api.nasa.gov/neo/rest/v1/'
 })
