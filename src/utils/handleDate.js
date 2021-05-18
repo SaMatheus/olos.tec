2e0770afd39820280013a7418298940fb28bad07
Matheus Sá <matheus.sa.alves@outlook.com>
2021-05-18 18:45:31 -0300

    Removendo console.log do codigo

--- src/utils/handleDate.js
+++ src/utils/handleDate.js
@@ -1,6 +1,5 @@
 // DATE-FNS
 import format from 'date-fns/format'
-import { addDays } from 'date-fns'
 
 const subtractedCurrentDay = (currentDay) => {
   const startDay = currentDay - 6
@@ -25,10 +24,8 @@ const subtractedCurrentYear = (currentMonth, currentYear) => {
 }
 
 const handleDate = () => {
-  // const currentDay = format(new Date(), 'dd')
-  const currentDay = 6
-  // const currentMonth = format(new Date(), 'MM')
-  const currentMonth = 1
+  const currentDay = format(new Date(), 'dd')
+  const currentMonth = format(new Date(), 'MM')
   const currentYear = format(new Date(), 'yyyy')
 
   if(currentDay) {
@@ -40,19 +37,14 @@ const handleDate = () => {
     let endDate = `${currentYear}-${currentMonth}-${currentDay}`
 
     if(currentDay - 6 >= 1) {
-      console.log("Mantendo o mês")
       startDate = `${currentYear}-${currentMonth}-${newStartDay}`
     }
     if(currentDay - 6 < 1) {
-      console.log("Mudando o mês")
       startDate = `${currentYear}-${newMonth}-${newDay}`
     }
     if(currentMonth - 1 < 1) {
-      console.log("Mudando o ano")
       startDate = `${newStartYear}-${newStartMonth}-${newDay}`
     }
-
-    console.log(startDate)
 
     return {
       startDate,
