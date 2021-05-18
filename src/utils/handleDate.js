92650c4a2bf76487addc6b5153ec90400f9b10b1
Matheus Sá <matheus.sa.alves@outlook.com>
2021-05-18 18:44:27 -0300

    Criando logica para pegar data final exata na primeira chama a API, independetemente do dia atual

--- src/utils/handleDate.js
+++ src/utils/handleDate.js
@@ -17,14 +17,24 @@ const subtractedCurrentMonth = (currentDay, currentMonth, currentYear) => {
   return { newDay, newMonth}
 }
 
+const subtractedCurrentYear = (currentMonth, currentYear) => {
+  const newStartMonth = String(Number(currentMonth) + 11)
+  const newStartYear = currentYear - 1
+  
+  return { newStartMonth, newStartYear}
+}
+
 const handleDate = () => {
-  const currentDay = format(new Date(), 'dd')
-  const currentMonth = format(new Date(), 'MM')
+  // const currentDay = format(new Date(), 'dd')
+  const currentDay = 6
+  // const currentMonth = format(new Date(), 'MM')
+  const currentMonth = 1
   const currentYear = format(new Date(), 'yyyy')
 
   if(currentDay) {
     const { newStartDay } = subtractedCurrentDay(currentDay)
     const { newDay, newMonth } = subtractedCurrentMonth(currentDay, currentMonth, currentYear)
+    const { newStartMonth, newStartYear } = subtractedCurrentYear(currentMonth, currentYear)
 
     let startDate = `${currentYear}-${currentMonth}-${newStartDay}`
     let endDate = `${currentYear}-${currentMonth}-${currentDay}`
@@ -37,6 +47,12 @@ const handleDate = () => {
       console.log("Mudando o mês")
       startDate = `${currentYear}-${newMonth}-${newDay}`
     }
+    if(currentMonth - 1 < 1) {
+      console.log("Mudando o ano")
+      startDate = `${newStartYear}-${newStartMonth}-${newDay}`
+    }
+
+    console.log(startDate)
 
     return {
       startDate,
