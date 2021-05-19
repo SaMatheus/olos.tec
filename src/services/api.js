59baea71ae76b914c07c14a88a93b14368430d47
Matheus Sá <matheus.sa.alves@outlook.com>
2021-05-19 13:48:39 -0300

    Melhoria: criando arquivo .env para lidar com dados sensiveis

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-export const apiKey = '4fUo1vx6eY0JIfeYoaMxrlGlWlM8ZpcoTzi7rQWs'
-
 const version = "v1"
 
 export const api = axios.create({
--- src/services/requestApi.js
+++ src/services/requestApi.js
@@ -1,10 +1,12 @@
 // REQUEST
-import { api, apiKey } from '../services/api'
+import { api } from '../services/api'
+
+const { REACT_APP_NASA_KEY } = process.env;
 
 const request = { 
   getAsteroids: async (startDate, endDate) => 
   await api
-    .get(`feed?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`)
+    .get(`feed?start_date=${startDate}&end_date=${endDate}&api_key=${REACT_APP_NASA_KEY}`)
     .then((response) => {
       return response.data.near_earth_objects
     })
